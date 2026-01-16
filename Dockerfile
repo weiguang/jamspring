29ae0b163c80ff9d59e302e136e65b38b3177c35
weiguang <chen2621978@gmail.com>
2026-01-16 10:23:04 +0800

    build(*): update jdk to 25

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM eclipse-temurin:21-jdk-alpine
+FROM eclipse-temurin:25-jdk-alpine
 
 # service path
 ENV SERVICE_PATH=/app/service
--- okayjam-web-core/src/test/java/com/okayjam/web/common/service/ServiceApplicationTests.java
+++ okayjam-web-core/src/test/java/com/okayjam/web/common/service/ServiceApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServiceApplicationTests {
 
-    @Resource("dbLockService")
+    @Resource
     LockService lockService;
 
     @Test
